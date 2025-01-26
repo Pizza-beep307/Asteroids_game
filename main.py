f9ab50cd1cdb2f1fdc9a3287ffc6fee78ba032b2
Pizza-beep307 <younes.chelda@gmail.com>
2025-01-26 15:43:29 +0100

    circle shape python file added for sprite

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from constants import *
 def main():
     pygame.init()
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
     #print(pygame.get_init()) to see if pygame is initialized
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,7 +17,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-        dt = pygame.time.Clock().tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
