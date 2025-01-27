88002c1ab59b8f17f96a85c3a880a27484f1ec96
Pizza-beep307 <younes.chelda@gmail.com>
2025-01-27 12:51:48 +0100

    player class fixed and used in main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     #print(pygame.get_init()) to see if pygame is initialized
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,7 +17,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
+        player.draw(screen)
+        pygame.display.flip()       
         dt = clock.tick(60) / 1000
 
 
